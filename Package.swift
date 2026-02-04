eb428e7c563fb5c5793e8bff9a33c58b94f94913
James Langdon <corporatelangdon@gmail.com>
2026-02-04 16:20:33 -0500

    add vldebuglogger implementation

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "VLDebugLogger",
+    platforms: [.iOS(.v16)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -12,11 +13,17 @@ let package = Package(
             targets: ["VLDebugLogger"]
         ),
     ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "1.2.1")),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "VLDebugLogger"
+            name: "VLDebugLogger",
+            dependencies: [.product(name: "Collections", package: "swift-collections")]
         ),
         .testTarget(
             name: "VLDebugLoggerTests",
